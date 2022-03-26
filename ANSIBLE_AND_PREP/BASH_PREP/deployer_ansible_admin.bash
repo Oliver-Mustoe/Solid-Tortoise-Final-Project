0b61fc38da7dc0084c8794b0f926d0f7295dc420
Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-03-26 15:47:53 -0400

    Bashy

--- ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
+++ ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
@@ -1,5 +1,5 @@
 # Script to setup Linux (Ubuntu) host for Ansible SSH deployment
-# Version: 0.4
+# Version: 0.5
 # Add deployer user
 sudo adduser deployer
 wait
@@ -10,10 +10,10 @@ sudo apt install ansible sshpass python3-paramiko -y
 wait
 
 # Create "sys265" file as the deployer user
-sudo su - deployer -c 'echo "deployer     ALL=(ALL)     NOPASSWD: ALL" >> sys265'
+echo "deployer     ALL=(ALL)     NOPASSWD: ALL" >> sys265
 
 # Copy sys265 to "/etc/sudoers.d/sys264" as the deployer user
-sudo su - deployer -c 'sudo cp sys265 /etc/sudoers.d/sys265'
+sudo cp sys265 /etc/sudoers.d/sys265
 
 # Generate SSH key for ansible as the deployer user
 sudo su - deployer -c 'ssh-keygen -t rsa -C "controller"'
@@ -22,8 +22,8 @@ sudo su - deployer -c 'ssh-keygen -t rsa -C "controller"'
 sudo ansible-galaxy collection install ansible.windows
 
 # Let user know what they now need to do
-echo "Will need to know run the comman 'ssh-copy-id -i ~/.ssh/mykey deployer@ansible1-oliver' to copy your SSH to Linux users"
-echo "For Window's hosts, you will need to refer to 'PS_PREP' "
+echo "[Will need to know run the comman 'ssh-copy-id -i ~/.ssh/mykey deployer@ansible1-oliver' to copy your SSH to Linux users]"
+echo "[For Window's hosts, you will need to refer to 'PS_PREP']"
 
 # Change to deployer
 sudo su - deployer
--- ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_receive.bash
+++ ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_receive.bash
@@ -1,5 +1,5 @@
 # Script to setup Linux (CentOS 7) hosts for Ansible SSH
-# Version: 0.4
+# Version: 0.5
 # Add deployer user
 sudo useradd -mk /etc/skel -s /bin/bash -d /home/deployer -G wheel deployer
 
@@ -8,10 +8,10 @@ sudo passwd deployer
 wait
 
 # Create "sys265" file as the deployer user
-sudo su - deployer -c 'echo "deployer     ALL=(ALL)     NOPASSWD: ALL" >> sys265'
+echo "deployer     ALL=(ALL)     NOPASSWD: ALL" >> sys265
 
 # Copy sys265 to "/etc/sudoers.d/sys264" as the deployer user
-sudo su - deployer -c 'sudo cp sys265 /etc/sudoers.d/sys265'
+sudo cp sys265 /etc/sudoers.d/sys265
 
 # Change to deployer
 sudo su - deployer
