00c3bc262e0ceff3fda1616a1e1486974af63b05
Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-03-27 18:20:02 -0400

    Fixing ansible install (2.9 --> 2.12)

--- ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
+++ ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
@@ -1,13 +1,9 @@
 # Script to setup Linux (Ubuntu) host for Ansible SSH deployment
-# Version: 1.0 - NEEDS DOUBLE CHECK
+# Version: 1.2 - NEEDS DOUBLE CHECK
 # Add deployer user
 sudo adduser deployer
 wait
 sudo usermod -aG sudo deployer
-
-# Install needed prerequisites
-sudo apt install ansible sshpass python3-paramiko -y
-wait
 
 # Create "sys265" file
 echo "deployer     ALL=(ALL)     NOPASSWD: ALL" >> sys265
@@ -20,6 +16,7 @@ sudo su - deployer -c 'ssh-keygen -t rsa -C "controller"'
 
 # Install needed Windows modules
 sudo ansible-galaxy collection install ansible.windows
+sudo ansible-galaxy collection install community.windows
 
 # Let user know what they now need to do
 echo "[Will need to know run the command 'ssh-copy-id -i ~/.ssh/mykey deployer@ansible1-oliver' to copy your SSH to Linux users]"
---
+++ ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_adminPREP.bash
@@ -0,0 +1,15 @@
+# Install pre-requisites for ansible
+# Version 0.1
+
+# Install needed prerequisites
+sudo apt install pip sshpass python3-paramiko -y
+wait
+
+# Installs Ansible
+sudo pip3 install ansible
+
+# Prompts user that system will now reboot
+read -p "Your system will now reboot..."
+
+# Reboot system
+sudo reboot now
