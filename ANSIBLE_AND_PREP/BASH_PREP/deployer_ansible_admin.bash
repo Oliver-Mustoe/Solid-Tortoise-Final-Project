3aae9d0250051fddcc2141e808c5ef851a197750
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-04-01 21:11:40 -0400

    Update deployer_ansible_admin.bash

--- ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
+++ ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
@@ -16,6 +16,7 @@ sudo cp sys265 /etc/sudoers.d/sys265
 sudo su - deployer -c 'ssh-keygen -t rsa -C "controller"'
 
 # Let user know what they now need to do
+ehco "[Will need to git clone https://Solid-Tortoise/Oliver-Mustoe/Solid-Tortoise]"
 echo "[Will need to know run the command 'ssh-copy-id -i ~/.ssh/mykey deployer@<HOST>' to copy your SSH to Linux users]"
 echo "[The scp command for mgmt02 to windows is 'scp ~/.ssh/id_rsa.pub <USERNAME>@<HOST>:/C:/ProgramData/ssh/administrators_authorized_keys']"
 echo "[Run 'Authorized_keys.ps1' on the windows host]"
