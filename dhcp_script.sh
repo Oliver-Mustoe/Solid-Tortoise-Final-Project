904e4c21ab02ae8881559a392f7c831fe0655ef9
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-04-07 00:25:43 -0400

    Update dhcp_script.sh

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -4,25 +4,16 @@
 
 ssh root@172.16.1.10 <<END
   yum install dhcp -y
-  cd /home/champuser
-  curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp01/dhcpd.conf > dhcpd.conf
-  mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
-  mv ./dhcpd.conf /etc/dhcp/dhcpd.conf.new
   cd /etc/dhcp/
-  mv ./dhcpd.conf.new ./dhcpd.conf
+  curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp01/dhcpd.conf > dhcpd.conf
   sudo systemctl start dhcpd
   sudo systemctl restart dhcpd
 END
 
 ssh root@172.16.1.11 <<END
   yum install dhcp -y
-  cd /home/champuser
-  curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp02/dhcpd.conf > dhcpd.conf
-  mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
-  mv ./dhcpd.conf /etc/dhcp/dhcpd.conf
-  mv ./dhcpd.conf /etc/dhcp/dhcpd.conf.new
   cd /etc/dhcp/
-  mv ./dhcpd.conf.new ./dhcpd.conf
+  curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp02/dhcpd.conf > dhcpd.conf
   sudo systemctl start dhcpd
   sudo systemctl restart dhcpd
 END
