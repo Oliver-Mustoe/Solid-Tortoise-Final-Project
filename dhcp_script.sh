0b7c6da031166ef58cee5dd4619b1c3dc40e3372
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-04-07 00:11:16 -0400

    Update dhcp_script.sh

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -2,17 +2,17 @@
 
 # Version 0.1
 
-ssh champuser@172.16.1.10 <<END
-  sudo -i
+ssh root@172.16.1.10 <<END
   curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp01/dhcpd.conf > dhcpd.conf
+  mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
   mv dhcpd.conf /etc/dhcp/dhcpd.conf
   sudo systemctl start dhcpd
   sudo systemctl restart dhcpd
 END
 
-ssh champuser@172.16.1.11 <<END
-  sudo -i
+ssh root@172.16.1.11 <<END
   curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp01/dhcpd.conf > dhcpd.conf
+  mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
   mv dhcpd.conf /etc/dhcp/dhcpd.conf
   sudo systemctl start dhcpd
   sudo systemctl restart dhcpd
