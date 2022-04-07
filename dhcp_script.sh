124900c69543bbb4e9ce19ff22db16b65bdfeef9
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-04-07 00:13:12 -0400

    Update dhcp_script.sh

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -3,6 +3,7 @@
 # Version 0.1
 
 ssh root@172.16.1.10 <<END
+  yum install dhcp -y
   curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp01/dhcpd.conf > dhcpd.conf
   mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
   mv dhcpd.conf /etc/dhcp/dhcpd.conf
@@ -11,6 +12,7 @@ ssh root@172.16.1.10 <<END
 END
 
 ssh root@172.16.1.11 <<END
+  yum install dhcp -y
   curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp01/dhcpd.conf > dhcpd.conf
   mv /etc/dhcp/dhcpd.conf /etc/dhcp/dhcpd.conf.bak
   mv dhcpd.conf /etc/dhcp/dhcpd.conf
