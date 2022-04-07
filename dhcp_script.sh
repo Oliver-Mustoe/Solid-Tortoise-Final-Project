084191c0da8c2630ed8bbc3cbe2b9fb70ffc7975
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-04-07 00:33:24 -0400

    Update dhcp_script.sh

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -8,6 +8,7 @@ ssh root@172.16.1.10 <<END
   curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp01/dhcpd.conf > dhcpd.conf
   systemctl start dhcpd
   systemctl restart dhcpd
+  systemctl enable dhcpd
   firewall-cmd --add-service=dhcp --permanent
   firewall-cmd --reload
 END
@@ -17,7 +18,8 @@ ssh root@172.16.1.11 <<END
   cd /etc/dhcp/
   curl https://raw.githubusercontent.com/Oliver-Mustoe/Solid-Tortoise-Final-Project/main/dhcp02/dhcpd.conf > dhcpd.conf
   systemctl start dhcpd
-  ystemctl restart dhcpd
+  systemctl restart dhcpd
+  systemctl enable dhcpd
   firewall-cmd --add-service=dhcp --permanent
   firewall-cmd --reload
 END
