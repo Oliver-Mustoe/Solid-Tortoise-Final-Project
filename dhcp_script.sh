7d6691b6408219a760ae89b825a7ff30f650ad27
ChampPG <71086240+ChampPG@users.noreply.github.com>
2022-04-08 19:54:13 -0400

    Update dhcp_script.sh

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -12,6 +12,7 @@ ssh root@172.16.1.10 <<END
   systemctl restart dhcpd
   systemctl enable dhcpd
   firewall-cmd --add-service=dhcp --permanent
+  firewall-cmd --add-port=647/udp --permanent
   firewall-cmd --reload
 END
 
@@ -23,5 +24,6 @@ ssh root@172.16.1.11 <<END
   systemctl restart dhcpd
   systemctl enable dhcpd
   firewall-cmd --add-service=dhcp --permanent
+  firewall-cmd --add-port=647/udp --permanent
   firewall-cmd --reload
 END
