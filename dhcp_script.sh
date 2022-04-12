d68179f3bb807fcd7973ce3c279c4fc9252510f7
Oliver-Mustoe <71083461+Oliver-Mustoe@users.noreply.github.com>
2022-04-12 17:26:41 -0400

    tcp

--- dhcp_script.sh
+++ dhcp_script.sh
@@ -12,7 +12,7 @@ ssh root@172.16.1.10 <<END
   systemctl restart dhcpd
   systemctl enable dhcpd
   firewall-cmd --add-service=dhcp --permanent
-  firewall-cmd --add-port=647/udp --permanent
+  firewall-cmd --add-port=647/tcp --permanent
   firewall-cmd --reload
   systemctl restart dhcpd
 END
@@ -25,7 +25,7 @@ ssh root@172.16.1.11 <<END
   systemctl restart dhcpd
   systemctl enable dhcpd
   firewall-cmd --add-service=dhcp --permanent
-  firewall-cmd --add-port=647/udp --permanent
+  firewall-cmd --add-port=647/tcp --permanent
   firewall-cmd --reload
   systemctl restart dhcpd
 END
