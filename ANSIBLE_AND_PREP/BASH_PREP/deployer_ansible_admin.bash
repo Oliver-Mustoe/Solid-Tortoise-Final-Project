fd56c7920278942d515a192486b5624b22ce9204
Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-04-13 11:43:46 -0400

    deployer --> hatchling

--- ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
+++ ANSIBLE_AND_PREP/BASH_PREP/deployer_ansible_admin.bash
@@ -1,30 +1,30 @@
 #!/bin/bash
 # Script to setup Linux (Ubuntu) host for Ansible SSH deployment
 # Version: 1.2 - NEEDS DOUBLE CHECK
-# Add deployer user
-sudo adduser deployer
+# Add hatchling user
+sudo adduser hatchling
 wait
-sudo usermod -aG sudo deployer
+sudo usermod -aG sudo hatchling
 
 # Create "sys265" file
-echo "deployer     ALL=(ALL)     NOPASSWD: ALL" >> sys265
+echo "hatchling     ALL=(ALL)     NOPASSWD: ALL" >> sys265
 
 # Copy sys265 to "/etc/sudoers.d/sys265"
 sudo cp sys265 /etc/sudoers.d/sys265
 
 # Install needed prerequisites
-sudo su - deployer -c 'sudo apt install pip sshpass python3-paramiko -y'
+sudo su - hatchling -c 'sudo apt install pip sshpass python3-paramiko -y'
 wait
 
 # Installs Ansible, (pip3 for python3)
-sudo su - deployer -c 'pip3 install ansible'
+sudo su - hatchling -c 'pip3 install ansible'
 wait
 
-# Generate SSH key for ansible as the deployer user
-sudo su - deployer -c 'ssh-keygen -t rsa'
+# Generate SSH key for ansible as the hatchling user
+sudo su - hatchling -c 'ssh-keygen -t rsa'
 
 # Let user know what they now need to do
-echo "[Will need to git clone https://Solid-Tortoise/Oliver-Mustoe/Solid-Tortoise for deployer]"
+echo "[Will need to git clone https://Solid-Tortoise/Oliver-Mustoe/Solid-Tortoise for hatchling]"
 
 # Prompts user that system will now reboot
 read -p "Your system will now reboot..."
