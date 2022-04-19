e30c7f745a038334fe05dcb80dced3d8367dd594
Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-04-18 22:53:07 -0400

    Starting docker and dfs

---
+++ ANSIBLE_AND_PREP/PS_PROD/NS_RG_ShellShare.ps1
@@ -0,0 +1,15 @@
+# This script creates a namespace and a replication group, place this scripts contents inside the ansible file (BACKUP IN CASE ANSIBLE DOES NOT WORK!!!)
+# Version 0.1 (WIP)
+
+# Make "shell_share" shared folder
+New-Item 'C:\DFSRoots\shell_share' -ItemType Directory
+
+New-SMBShare -Name shell_share -Path 'C:\DFSRoots\shell_share' ChangeAccess "tortoise\Domain Users"
+
+# Create the namespace "shell_share" (path is share, targetpath is namespace)
+New-DFsnRoot -TargetPath "\\MGMT01\shell_share" -Type DomainV2 -Path "C:\DFSRoots\shell_share"
+
+# Create folder in namespace
+New-DfsnFolder -Path "\\MGMT01\shell_share\user_info"
+
+# Add folder targets
