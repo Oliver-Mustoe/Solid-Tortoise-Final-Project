89edd1b419a55782e6ee9ed7e9d4f70eb1f46da1
Mustoe <oliver.mustoe@mymail.champlain.edu>
2022-04-22 14:04:15 -0400

    powershell incase the ansible don't work

--- ANSIBLE_AND_PREP/PS_PROD/PS_PROD/NS_RG_ShellShare.ps1
+++ ANSIBLE_AND_PREP/PS_PROD/PS_PROD/NS_RG_ShellShare.ps1
@@ -1,7 +1,7 @@
 # This script creates a namespace and a replication group, place this scripts contents inside the ansible file (BACKUP IN CASE ANSIBLE DOES NOT WORK!!!)
 # Version 0.1 (WIP)
 
-New-SMBShare -Name shell_share -Path 'C:\DFSRoots\shell_share' -ChangeAccess "tortoise\Domain Users"
+New-SMBShare -Name shell_share -Path 'C:\DFSRoots\shell_share' -FullAccess "tortoise\Domain Admins" -ReadAccess "tortoise\Domain Users"
 
 # Create the namespace "shell_share" | path is namespace path,domain, targetpath is individual share path,local
 New-DFsnRoot -TargetPath "C:\DFSRoots\shell_share" -Type DomainV2 -Path "\\tortoise.local\shell_share"
